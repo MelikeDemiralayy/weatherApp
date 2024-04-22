2363b49ccda3d037d5be27e363ccf846ace76454
MelikeDemiralay <melikedemiralay3215@gmail.com>
2024-04-22 21:53:40 +0300

    Update styles for WeatherApp and Search components

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -15,16 +15,20 @@ function Search({ getWeather }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label className="text-blue-500 block mb-2">Enter City:</label>
-      <input 
-        type="text" 
-        className="w-full px-4 py-2 rounded-lg border border-white mb-4" 
-        value={city} 
-        onChange={handleInputChange} 
+      <div className="relative mb-2 flex justify-center">
+        <img src="/public/assets/Title.svg" alt="Title" className="mx-auto" />
+      </div>
+      <input
+        type="text"
+        className="w-[350px] h-[56px] px-20 py-0 rounded-lg border border-white mb-4 bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent mx-auto"
+        value={city}
+        onChange={handleInputChange}
       />
-      <button 
-        type="submit" 
-        className="bg-blue-500 text-white px-4 py-2 rounded-lg mb-4"
+
+      <button
+        type="submit"
+        className="bg-gray-800 text-white px-4 py-2 rounded-lg mb-4 hover:bg-gray-400  hover:text-white transition duration-300"
+        style={{ display: "block" }}
       >
         Get Weather
       </button>
